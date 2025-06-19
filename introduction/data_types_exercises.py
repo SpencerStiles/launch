864ba4cdfd031286f802903adea066c63befa62f
Spencer Stiles <spencer.stiles12@gmail.com>
2025-06-18 20:52:38 -0600

    Finished Introduction to Programming - Data Types exercises

---
+++ introduction/data_types_exercises.py
@@ -0,0 +1,20 @@
+'True' = string
+False = boolean
+(1, 2, 3) = tuple
+1.5 = float
+[1, 2, 3] = list
+2 = int
+range(5) = range
+{1, 2, 3} = set
+None = None
+{'foo': 'bar'} = dictionary
+
+names = ('Asta', 'Butterscotch', 'Pudding', 'Neptune', 'Darwin')
+
+pets = {
+    'Asta': 'dog',
+    'Butterscotch': 'cat',
+    'Pudding': 'cat',
+    'Neptune': 'fish',
+    'Darwin': 'lizard'
+}
